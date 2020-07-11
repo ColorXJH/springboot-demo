604f104984fcf3c4390609de9860c83d53a956b2
Master <1204332316@qq.com>
2020-07-11 19:22:16 +0800

    springboot2.0练习

--- springboot-validation-filter/src/main/java/com/master/controller/NewController.java
+++ springboot-validation-filter/src/main/java/com/master/controller/NewController.java
@@ -1,10 +1,13 @@
 package com.master.controller;
 
+import com.master.model.OtherBean;
 import com.master.model.UserBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * @author ColorXJH
@@ -17,11 +20,15 @@ public class NewController {
     @Autowired
     private UserBean properties;
 
+    @Resource
+    private OtherBean other;
+
     @Value("${colorxjh.name}")
     private String name;
     @GetMapping("/testYML")
     public void testYML(){
         System.out.println(name);
         System.out.println(properties);
+        System.out.println(other);
     }
 }
