797ce6ab051f279d525b1d84aa59d6881b69fc32
Master <1204332316@qq.com>
2020-07-13 17:19:14 +0800

    springboot2.0练习

--- springboot-thymeleaf/src/main/java/com/master/controller/ThymeleafController.java
+++ springboot-thymeleaf/src/main/java/com/master/controller/ThymeleafController.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -47,5 +48,15 @@ public class ThymeleafController {
     @GetMapping("/getFragment")
     public String getFragment(){
         return "fragment";
+    }
+
+    @RequestMapping("/layout")
+    public String layout() {
+        return "layout";
+    }
+
+    @RequestMapping("/homepage")
+    public String home() {
+        return "newpage";
     }
 }
